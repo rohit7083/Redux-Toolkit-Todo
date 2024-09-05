f92c674a13f74d162025d6b952ef8958f4ac8181
MyTution <rohitsonawane889@gmail.com>
2024-09-05 20:04:50 +0530

    all about Redux

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddTodo from './Compo/AddTodo'
+import Todos from './Compo/Todos'
+function App() {
+
+  return (
+    <>
+  <h1>learn about redux ttols</h1>
+  <AddTodo/>
+  <Todos/>
+    </>
+  )
+}
+
+export default App
